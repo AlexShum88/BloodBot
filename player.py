f8e0f7182c18229826266dc406483a66381713c9
Elanor <elentin88@gmail.com>
2020-05-07 11:06:28 +0300

    start

---
+++ player.py
@@ -0,0 +1,17 @@
+"""файл класу гравця де його методи реєстрації, дій,"""
+
+class Player:
+   def __init__(self, name, clan, blood):
+       self.isill = False
+       return
+   def register(self):
+       return
+   def set_dysciplines(self):
+       return
+   def eating(self):
+       return
+   def hunting(self):
+       return
+
+
+
